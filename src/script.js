471d7b861909bea6726b7cbdb23e3af5a57b9363
beginner1986 <beginner1986@gmail.com>
2021-08-17 17:10:20 +0200

    Launch button activation conditions implemented.

--- src/script.js
+++ src/script.js
@@ -9,14 +9,34 @@ function checkPassword() {
 function enableInputs() {
     const checkButtons = document.querySelectorAll('input[type="checkbox"]');
     checkButtons.forEach(enableElement);
+    checkButtons.forEach(cb => cb.addEventListener("change", function() {
+        checkLaunchConditions();
+    }));
 
     const levers = document.querySelectorAll('input[type="range"]');
     levers.forEach(enableElement);
-
-    const launchBtn = document.getElementById('launch-btn');
-    enableElement(launchBtn);
+    levers.forEach(cb => cb.addEventListener("change", function() {
+        checkLaunchConditions();
+    }));
 }
 
 function enableElement(element) {
     element.disabled = false;
+}
+
+function checkLaunchConditions() {
+    let count = 0;
+
+    const checkButtons = document.querySelectorAll('input[type="checkbox"]');
+    checkButtons.forEach(cb => { if(cb.checked) count++});
+
+    const levers = document.querySelectorAll('input[type="range"]');
+    levers.forEach(cb => { if(cb.value >= 99) count++ });
+
+    const launchBtn = document.getElementById("launch-btn");
+    if(count === 11) {
+        launchBtn.disabled = false;
+    } else {
+        launchBtn.disabled = true;
+    }
 }
